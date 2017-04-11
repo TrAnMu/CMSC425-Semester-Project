4dbcb555090898eb4ab6114b075f30fdd4cff15c
cford1 <corbin.ford@yahoo.com>
2017-04-11 18:41:14 -0400

    First Commit

---
+++ Assets/Scripts/NoteController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteController : MonoBehaviour {
+
+    public Transform redNote;
+    public Transform blueNote;
+    public Transform greenNote;
+    public Transform yellowNote;
+    public Transform orangeNote;
+    public Transform objectToCreate;
+
+    public bool resetTimer = true;
+    public float posX;
+
+    List<float> testNotes = new List<float>() { 1, 5, 3, 4, 2, 5, 2, 1, 2, 3, 5, 5, 4, 5, 5, 1, 2, 4, 1, 1, 4, 3 };
+    public int notePos = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(resetTimer == true)
+        {
+            StartCoroutine(createNote());
+            resetTimer = false;
+        }
+		
+	}
+
+    IEnumerator createNote()
+    {
+        yield return new WaitForSeconds(1);
+
+        if(testNotes[notePos] == 1)
+        {
+            posX = -1.35f;
+            objectToCreate = greenNote;
+        }
+        if(testNotes[notePos] == 2)
+        {
+            posX = -0.65f;
+            objectToCreate = redNote;
+        }
+        if(testNotes[notePos] == 3)
+        {
+            posX = 0f;
+            objectToCreate = yellowNote;
+        }
+        if(testNotes[notePos] == 4)
+        {
+            posX = 0.7f;
+            objectToCreate = blueNote;
+        }
+        if(testNotes[notePos] == 5)
+        {
+            posX = 1.4f;
+            objectToCreate = orangeNote;
+        }
+        notePos++;
+        resetTimer = true;
+        Instantiate(objectToCreate, new Vector3(posX, 2.55f, 3.28f), objectToCreate.rotation);
+    }
+}
---
+++ Assets/Scripts/NoteDetector.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteDetector : MonoBehaviour {
+
+    public Transform noteBurst;
+    public Transform redBurst;
+    public Transform blueBurst;
+    public Transform greenBurst;
+    public Transform yellowBurst;
+    public Transform orangeBurst;
+    public Transform blackBurst;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "FailBox")
+        {
+            Destroy(gameObject);
+            Instantiate(blackBurst, transform.position, blackBurst.rotation);
+            
+        } if(other.gameObject.name == "greenTrigger")
+        {
+            Destroy(gameObject);
+            Instantiate(greenBurst, transform.position, greenBurst.rotation);
+        }
+         if (other.gameObject.name == "redTrigger")
+        {
+            Destroy(gameObject);
+            Instantiate(redBurst, transform.position, redBurst.rotation);
+        }
+        if (other.gameObject.name == "yellowTrigger")
+        {
+            Destroy(gameObject);
+            Instantiate(yellowBurst, transform.position, yellowBurst.rotation);
+        }
+        if (other.gameObject.name == "blueTrigger")
+        {
+            Destroy(gameObject);
+            Instantiate(blueBurst, transform.position, blueBurst.rotation);
+        }
+        if (other.gameObject.name == "orangeTrigger")
+        {
+            Destroy(gameObject);
+            Instantiate(orangeBurst, transform.position, orangeBurst.rotation);
+        }
+    }
+}
