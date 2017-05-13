7c76fd22695ee8ec197ce8f49ca7ee42355a3f87
cford1 <corbin.ford@yahoo.com>
2017-05-13 15:41:18 -0400

    Visualizer
    Hope this works

---
+++ Assets/Scripts/AudioAnalyzer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class AudioAnalyzer : MonoBehaviour {
+    AudioSource audioSource;
+    public static float[] sampleData = new float[512];
+    public static float[] frequenvyBands = new float[8];
+
+
+	// Use this for initialization
+	void Start () {
+
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetSpectrumAudioSource();
+	}
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(sampleData, 0, FFTWindow.Blackman);
+    }
+
+    void CreateFrequencyBands()
+    {
+
+    }
+}
---
+++ Assets/Scripts/CreateVisualData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateVisualData : MonoBehaviour {
+    public GameObject cubePrefab;
+    GameObject[] cubes = new GameObject[512];
+    public float maxScale;
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i < cubes.Length; i++)
+        {
+            GameObject cubeInstance = (GameObject)Instantiate(cubePrefab);
+            cubeInstance.transform.position = this.transform.position;
+            cubeInstance.transform.parent = this.transform;
+            cubeInstance.name = "Cube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            cubeInstance.transform.position = Vector3.forward * 100;
+            cubes[i] = cubeInstance;
+        }
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int ctr = 0; ctr < cubes.Length; ctr++)
+        {
+            if(cubes != null)
+            {
+                cubes[ctr].transform.localScale = new Vector3(10, (AudioAnalyzer.sampleData[ctr] * maxScale) +2 , 10);
+            }
+        }
+    }
+}
